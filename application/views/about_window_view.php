ad0ba89e8814356f39b2a6979378c17513043d1c
itek102 <andronov214@gmail.com>
2016-11-26 01:30:41 +0400

    Contact+Catalog

---
+++ application/views/about_window_view.php
@@ -0,0 +1,17 @@
+    <div class="col-lg-12 about-window">
+        <div class="row"><h3 class="title-work">НЕМНОГО <SPAN CLASS='green-title'>О ОКНАХ</SPAN></h3></div>
+        <div class="row">
+           <div class="col-lg-1"></div>
+            <div class="col-lg-2">
+                <img src="images/catalog/window.JPG" alt="" class="img-responsive">
+            </div>
+            <div class="col-lg-8">
+            <span class='about-window-blue about-window-margin'>ТИШИНА И ТЕПЛО КАК МИНИМУМ 50 ЛЕТ!</span>
+            <p class="about-window-text">Окна REHAU даже в самую лютую зиму превратят ваш дом в оазис тепла и уюта и защитят от холода не хуже кирпичной стены. Они надежно блокируют проникновение холода, ветра и атмосферных осадков.</p>
+            <p class="about-window-text">
+                Так, окно из профиля <span class="about-window-blue">GENEO</span> с 6-камерным строением и системной глубиной 86 мм по теплотехническим характеристикам эквивалентно кирпичной стене толщиной 1,3 метра!
+            </p>
+            </div>
+            <div class="col-lg-1"></div>
+        </div>
+    </div>
---
+++ application/views/catalog_view.php
@@ -0,0 +1,78 @@
+   <div class="col-lg-12 catalog">
+        <h3 class="title-work title-cat">НАШ АССОРТИМЕНТ <SPAN CLASS='green-title'>ПЛАСТИКОВЫХ ОКОН</SPAN></h3>
+        <div class="item2 col-lg-4">
+				<div class="thumbnail">
+					<img src="images/catalog/1.jpg" alt="" class="img-responsive">
+					<div class="caption">
+						<p class="cat-plastic">Пластиковое окно</p>
+						<p class="green-title cat-title">REHAU GENEO</p>
+						<a href="#" class="btn-cat-next btn btn-primary">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+			</div>
+			<div class="item2 col-lg-4">
+				<div class="thumbnail">
+					<img src="images/catalog/2.jpg" alt="" class="img-responsive">
+					<div class="caption">
+						<p class="cat-plastic">Пластиковое окно</p>
+						<p class="green-title cat-title">REHAU GENEO</p>
+						<a href="#" class="btn-cat-next btn btn-primary">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+			</div><div class="item2 col-lg-4">
+				<div class="thumbnail">
+					<img src="images/catalog/1.jpg" alt="" class="img-responsive">
+					<div class="caption">
+						<p class="cat-plastic">Пластиковое окно</p>
+						<p class="green-title cat-title">REHAU GENEO</p>
+						<a href="#" class="btn-cat-next btn btn-primary">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+			</div><div class="item2 col-lg-4">
+				<div class="thumbnail">
+					<img src="images/catalog/2.jpg" alt="" class="img-responsive">
+					<div class="caption">
+						<p class="cat-plastic">Пластиковое окно</p>
+						<p class="green-title cat-title">REHAU GENEO</p>
+						<a href="#" class="btn-cat-next btn btn-primary">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+			</div><div class="item2 col-lg-4">
+				<div class="thumbnail">
+					<img src="images/catalog/2.jpg" alt="" class="img-responsive">
+					<div class="caption">
+						<p class="cat-plastic">Пластиковое окно</p>
+						<p class="green-title cat-title">REHAU GENEO</p>
+						<a href="#" class="btn-cat-next btn btn-primary">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+			</div><div class="item2 col-lg-4">
+				<div class="thumbnail">
+					<img src="images/catalog/1.jpg" alt="" class="img-responsive">
+					<div class="caption">
+						<p class="cat-plastic">Пластиковое окно</p>
+						<p class="green-title cat-title">REHAU GENEO</p>
+						<a href="#" class="btn-cat-next btn btn-primary">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+			</div><div class="item2 col-lg-4">
+				<div class="thumbnail">
+					<img src="images/catalog/2.jpg" alt="" class="img-responsive">
+					<div class="caption">
+						<p class="cat-plastic">Пластиковое окно</p>
+						<p class="green-title cat-title">REHAU GENEO</p>
+						<a href="#" class="btn-cat-next btn btn-primary">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+			</div><div class="item2 col-lg-4">
+				<div class="thumbnail">
+					<img src="images/catalog/1.jpg" alt="" class="img-responsive">
+					<div class="caption">
+						<p class="cat-plastic">Пластиковое окно</p>
+						<p class="green-title cat-title">REHAU GENEO</p>
+						<a href="#" class="btn-cat-next btn btn-primary">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+			</div>
+			
+    </div>
